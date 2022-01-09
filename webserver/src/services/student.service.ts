0d6b92f4fa6685fb27265a4a5b143faddd9b2d37
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-01-09 17:13:02 -0500

    Student services

--- webserver/src/services/student.service.ts
+++ webserver/src/services/student.service.ts
@@ -1,1 +1,58 @@
-export const app = {};
+import axios from "axios"
+import { getAuthorizationToken } from "../libs/tokenInterceptor";
+import { URI } from "../libs/utils"
+import { Student, studentConverter } from "../models/Student";
+
+export const getStudents = async () => {
+    return await axios.get(`${URI}/students`,
+        {
+            headers: {
+                Authorization: getAuthorizationToken()
+            }
+        });
+}
+
+export const createStudent = async (student: Student) => {
+    return await axios.post(`${URI}/students`, studentConverter.toJSON(student),
+        {
+            headers: {
+                Authorization: getAuthorizationToken()
+            }
+        });
+}
+
+export const deleteStudents = async () => {
+    return await axios.delete(`${URI}/students`,
+        {
+            headers: {
+                Authorization: getAuthorizationToken()
+            }
+        });
+}
+
+export const getStudent = async (id: any) => {
+    return await axios.get(`${URI}/students/student/${id}`,
+        {
+            headers: {
+                Authorization: getAuthorizationToken()
+            }
+        });
+}
+
+export const updateStudent = async (id: string, student: Student) => {
+    return await axios.put(`${URI}/students/student/${id}`, studentConverter.toJSON(student),
+        {
+            headers: {
+                Authorization: getAuthorizationToken()
+            }
+        });
+}
+
+export const deleteStudent = async (id: any) => {
+    return await axios.delete(`${URI}/students/student/${id}`,
+        {
+            headers: {
+                Authorization: getAuthorizationToken()
+            }
+        });
+}
