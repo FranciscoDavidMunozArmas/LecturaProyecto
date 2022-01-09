b6effe9511e7a04bfae29a9a9dda85798497c6c3
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-01-09 16:02:55 -0500

    Firebase auth config

--- webserver/src/config/firebase.config.ts
+++ webserver/src/config/firebase.config.ts
@@ -1,1 +1,15 @@
-export const app = {};
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { CONSTANTS } from "../libs/utils";
+
+const firebaseConfig = {
+    apiKey: CONSTANTS.FIREBASE_API_KEY,
+    authDomain: CONSTANTS.FIREBASE_AUTH_DOMAIN,
+    projectId: CONSTANTS.FIREBASE_PROJECT_ID,
+    storageBucket: CONSTANTS.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: CONSTANTS.FIREBASE_MESSAGING_SENDER_ID,
+    appId: CONSTANTS.FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
