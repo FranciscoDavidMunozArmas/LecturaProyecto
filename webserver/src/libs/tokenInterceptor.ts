7f4443b4f8c786230b9eee48c39e2fa7114f9958
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-01-09 16:10:37 -0500

    Token Interceptor

---
+++ webserver/src/libs/tokenInterceptor.ts
@@ -0,0 +1,46 @@
+import { CONSTANTS } from "./utils";
+import Cookies from "cookies-ts";
+import jwt from 'jsonwebtoken';
+
+export const setUpToken = (token: any, storage?: boolean) => {
+    if (!storage) {
+        const cookies = new Cookies();
+        const exipireDate = new Date();
+        exipireDate.setHours(exipireDate.getHours() + 2);
+        cookies.set(CONSTANTS.TOKEN_NAME as string, token, {
+            expires: exipireDate
+        });
+    } else {
+        localStorage.setItem(CONSTANTS.TOKEN_NAME as string, token);
+    }
+}
+
+export const removeToken = () => {
+    const cookies = new Cookies();
+    cookies.remove(CONSTANTS.TOKEN_NAME as string);
+    localStorage.removeItem(CONSTANTS.TOKEN_NAME as string);
+}
+
+export const getToken = () => {
+    const cookies = new Cookies();
+    const cookieResult = cookies.get(CONSTANTS.TOKEN_NAME as string);
+    if (cookieResult) {
+        return cookieResult;
+    } else {
+        const localResult = localStorage.getItem(CONSTANTS.TOKEN_NAME as string);
+        return localResult;
+    }
+}
+
+export const checkToken = () => {
+    const cookies = new Cookies();
+    return !!localStorage.getItem(CONSTANTS.TOKEN_NAME as string) || !!cookies.get(CONSTANTS.TOKEN_NAME as string);
+}
+
+export const decodeToken = (token: any) => {
+    return jwt.verify(token, CONSTANTS.SECRET_KEY as string);
+}
+
+export const getAuthorizationToken = () => {
+    return `Bearer ${getToken()}`;
+}
