fca3d61ce88d5cbcbbddf0e0d1ad7ff3eb137994
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-01-09 16:54:17 -0500

    Certificate services

--- webserver/src/services/certificate.service.ts
+++ webserver/src/services/certificate.service.ts
@@ -1,1 +1,49 @@
-export const app = {};
+import axios from "axios"
+import { getAuthorizationToken } from "../libs/tokenInterceptor";
+import { URI } from "../libs/utils"
+
+
+export const getCertificates = async () => {
+    return await axios.get(`${URI}/certificates`,
+        {
+            headers: {
+                Authorization: getAuthorizationToken()
+            }
+        });
+}
+
+export const createCertificate = async (course: any) => {
+    return await axios.post(`${URI}/certificates`, course,
+        {
+            headers: {
+                Authorization: getAuthorizationToken()
+            }
+        });
+}
+
+export const deleteCertificates = async () => {
+    return await axios.delete(`${URI}/certificates`,
+        {
+            headers: {
+                Authorization: getAuthorizationToken()
+            }
+        });
+}
+
+export const getCertificate = async (id: any) => {
+    return await axios.get(`${URI}/certificates/certificate/${id}`,
+        {
+            headers: {
+                Authorization: getAuthorizationToken()
+            }
+        });
+}
+
+export const deleteCertificate = async (id: any) => {
+    return await axios.delete(`${URI}/certificates/certificate/${id}`,
+        {
+            headers: {
+                Authorization: getAuthorizationToken()
+            }
+        });
+}   
