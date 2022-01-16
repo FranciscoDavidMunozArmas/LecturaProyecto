eecc6b0414f9f1647ac63a24264e681c3d8a058e
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-01-15 19:41:04 -0500

    Subrouting

--- webserver/src/pages/Appmain.tsx
+++ webserver/src/pages/Appmain.tsx
@@ -1,22 +1,34 @@
 import React, { useEffect } from 'react'
-import { useNavigate } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import { checkToken } from '../libs/tokenInterceptor';
+import Certificate from './subpages/Certificate';
+import Course from './subpages/Course';
+import Home from './subpages/Home';
+import MyCourses from './subpages/MyCourses';
+import PlayCourse from './subpages/PlayCourse';
 
 function Appmain() {
 
     const navigate = useNavigate();
 
     useEffect(() => {
-        if(!checkToken()) {
+        if (!checkToken()) {
             navigate("/login");
         }
-        return () => {}
+        return () => { }
     }, [])
 
     return (
-        <div>
-            Main
-        </div>
+        <>
+            <Routes>
+                <Route path="home" element={<Home />} />
+                <Route path="mycourses" element={<MyCourses />} />
+                <Route path="certificates" element={<Certificate />} />
+                <Route path="course" element={<Course />} />
+                <Route path="course/:course" element={<PlayCourse />} />
+                <Route path="*" element={<Navigate to="home" />} />
+            </Routes>
+        </>
     )
 }
 
