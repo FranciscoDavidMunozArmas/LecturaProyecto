7c23345d6116b0174237e0c8a1d116b18f0526e2
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-01-20 08:25:26 -0500

    PlayerComponent

--- webserver/src/components/PlayerControls.tsx
+++ webserver/src/components/PlayerControls.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import { ArrowBackIos, ArrowForwardIos, Pause, PlayArrow } from '@material-ui/icons';
+import React, { useState } from 'react';
+import { palette } from '../libs/styles';
 
-function PlayerControls() {
-  return <div></div>;
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row' as const,
+    justifyContent: 'space-between' as const,
+    alignItems: 'center' as const,
+    padding: '0.5rem',
+  },
+  button: {
+    borderRadius: '100%',
+    border: 'none',
+    backgroundColor: palette.primary,
+    color: palette.white,
+  },
+
+}
+
+interface Props {
+  onPlay: () => void;
+  onPause: () => void;
+  onNext: () => void;
+  onPrevious: () => void;
+}
+
+function PlayerControls(props: Props) {
+
+  const [playState, setplayState] = useState<boolean>(true);
+
+  const handlePlay = () => {
+    setplayState(!playState);
+    playState ? props.onPause() : props.onPlay();
+  }
+
+  const handleNext = () => {
+    props.onNext();
+  }
+
+  const handlePrevious = () => {
+    props.onPrevious();
+  }
+
+  return (<>
+    <div style={styles.container}>
+      <button style={styles.button} onClick={handlePrevious}><ArrowBackIos /></button>
+      {
+        (!playState)
+          ? <button style={styles.button} onClick={handlePlay} ><PlayArrow /></button>
+          : <button style={styles.button} onClick={handlePlay}><Pause /></button>
+      }
+      <button style={styles.button} onClick={handleNext}><ArrowForwardIos /></button>
+    </div>
+  </>);
 }
 
 export default PlayerControls;
