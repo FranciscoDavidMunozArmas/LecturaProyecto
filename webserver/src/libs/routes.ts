80bae5b9dba67cb6ef326c31e5c35cf09b021614
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-02-01 12:00:39 -0500

    Teacher Teacher pages

--- webserver/src/libs/routes.ts
+++ webserver/src/libs/routes.ts
@@ -1,6 +1,12 @@
 import { CERTIFICATES_NAME, HOME_NAME, MY_COURSES_NAME, PATH_CERTIFICATES, PATH_HOME, PATH_MY_COURSES, SUBSECTION_HOME_1_NAME, SUBSECTION_HOME_2_NAME, SUBSECTION_HOME_3_NAME, SUBSECTION_MY_COURSES_1_NAME, SUBSECTION_MY_COURSES_2_NAME } from "./utils";
 
 export const routes: any = {
+    teacher: [
+        {
+            text: HOME_NAME,
+            path: PATH_HOME,
+        }
+    ],
     children: [
         {
             text: HOME_NAME,
--- webserver/src/pages/TeacherPage.tsx
+++ webserver/src/pages/TeacherPage.tsx
@@ -1,7 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { useSpeechSynthesis } from 'react-speech-kit';
+import Menubar from '../components/Menubar';
+import { TeacherProvider } from '../context/TeacherContext';
+import { routes } from '../libs/routes';
+import { palette } from '../libs/styles';
+import { toastManager } from '../libs/toastManager';
+import { checkToken, decodeToken, getToken } from '../libs/tokenInterceptor';
+import { GETTING_DATA_ERROR, PATH_COURSE, PATH_HOME, PATH_PLAYCOURSE, VOICE_ES } from '../libs/utils';
+import { Teacher, teacherConverter } from '../models/Teacher';
+import { getTeacher } from '../services/teacher.service';
+import CoursePage from './subpages/teacher/CoursePage';
+import Home from './subpages/teacher/Home';
+import PlayCourse from './subpages/teacher/PlayCourse';
+
+const styles = {
+    container: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row' as const,
+        height: '100vh',
+        backgroundColor: palette.white
+    },
+    menuContainer: {
+        width: '20%',
+        minWidth: '250px',
+        boxShadow: "2px 0 5px rgba(0,0,0,0.25)",
+        padding: '1rem',
+    },
+    contentContainer: {
+        width: '80%',
+        minWidth: '750px',
+        padding: '1rem',
+    },
+}
 
 function TeacherPage() {
-  return <div></div>;
+
+    const [currentTeacher, setCurrentTeacher] = useState<Teacher>();
+
+    const naviate = useNavigate();
+    const { speak, cancel } = useSpeechSynthesis();
+
+
+    const getData = async () => {
+        const token: any = decodeToken(getToken());
+        try {
+            const data = await getTeacher(token.token);
+            setCurrentTeacher(teacherConverter.fromJSON(data.data));
+        } catch (error: any) {
+            toastManager.error(GETTING_DATA_ERROR);
+            onSpeak(GETTING_DATA_ERROR);
+        }
+    }
+
+    const onSpeak = (text: string) => {
+        speak({ text, voice: VOICE_ES, rate: 1.2 });
+    }
+
+    useEffect(() => {
+        if (!checkToken()) {
+            naviate('/login');
+        }
+        getData();
+        return () => { };
+    }, []);
+
+
+    return (
+        <>
+            <TeacherProvider teacher={currentTeacher}>
+                <div style={styles.container}>
+                    <div style={styles.menuContainer}>
+                        <Menubar children={routes.teacher} />
+                    </div>
+                    <div style={styles.contentContainer}>
+                        <Routes>
+                            <Route path={PATH_HOME} element={<Home />} />
+                            <Route path={PATH_COURSE} element={<CoursePage />} />
+                            <Route path={`${PATH_COURSE}/${PATH_PLAYCOURSE}`} element={<PlayCourse />} />
+                            <Route path="/*" element={<Navigate to={PATH_HOME} />} />
+                        </Routes>
+                    </div>
+                </div>
+            </TeacherProvider>
+        </>
+    );
 }
 
 export default TeacherPage;
