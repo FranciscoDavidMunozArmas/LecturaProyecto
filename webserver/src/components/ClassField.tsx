31e5819ce9cb74829c9ca2aa28e2933f7a791b2a
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2022-02-05 10:01:36 -0500

    ClassField

--- webserver/src/components/ClassField.tsx
+++ webserver/src/components/ClassField.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { BORDER_RADIOUS, text } from '../libs/styles';
+import { CourseClass } from '../models/CourseClass';
 
-function ClassField() {
-  return <div></div>;
+const styles = {
+    container: {
+        width: '100%',
+        height: 'auto',
+        margin: '5px 10px',
+        padding: '10px 20px',
+        display: 'flex',
+        flexDirection: 'column' as const,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: BORDER_RADIOUS,
+    },
+    text: {
+        textAlign: 'start' as const,
+        width: '100%',
+        fontSize: text.subtitle.fontSize,
+    },
+}
+
+interface Props {
+    classCourse: CourseClass;
+}
+
+function ClassField(props: Props) {
+    return (<>
+        <div style={styles.container}>
+            <h6 style={styles.text}>
+                {props.classCourse.name}
+            </h6>
+        </div>
+    </>);
 }
 
 export default ClassField;
